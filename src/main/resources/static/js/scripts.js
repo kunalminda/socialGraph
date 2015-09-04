de4e4248fe93790acfc030cff611b40dc868392a
Saloni Jain <saloni.jain@snapdeal.com>
2015-09-05 00:43:39 +0530

    updated code

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,92 +1,46 @@
-$(document).ready(function() {
-  // $('[data-toggle=offcanvas]').click(function() {
-  //   $('.row-offcanvas').toggleClass('active');
-  // });
-
-  //start of carousel script
- //  	$('#myCarousel').carousel({
-	// interval: 10000
-	// })
-    
- //    $('#myCarousel').on('slid.bs.carousel', function(e) {
-    	// alert("slid"+e.currentTarget.id);
-//	});
-
-  //end of carousel script
-
-//  STUDENT_METHOD.loadStudentData();
-
-    $('#autocomplete-ajax').autocomplete({
-    serviceUrl: "http://localhost/SocialGraph/data/autocom.json",
-    onSelect: function (suggestion) {
-        alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
-    }
-	});
-
-	//First graph
- //     alchemy.begin({
- //        dataSource: "data/charlize.json",
- //        nodeCaption: 'name',
- //        nodeMouseOver: 'name',
- //        cluster: true,
- //        clusterColours: ["#1B9E77","#D95F02","#7570B3","#E7298A","#66A61E","#E6AB02"]});
-	// });
-
-  
-       $.ajax({
-           type: "POST",
-            url: "http://10.10.101.76:8080/submitQuery",
-            beforeSend: function(xhr){xhr.setRequestHeader('content-type', 'application/json');},
-            data:"MATCH (n) WHERE has(n.name) RETURN n.name AS name",
-            success: function(result) {
-              console.log(result);
-             },
-             error:function(result){
-             console.log(result);
-             }
-          });
-
-	//second graph
-	var config = {
-      dataSource: 'data/charlize.json',
-      forceLocked: false,
-      linkDistance: function(){ return 40; },
-
-      nodeTypes: {"type":["movie", 
-                          "award", 
-                          "person"]},
-      caption: function(node){ 
-          return node.caption; 
-      }
-    };
-
-    alchemy = new Alchemy(config);
-
-
-// var STUDENT_METHOD ={
-
-//         handlerData:function(resJSON){
-        	
-//             var templateSource   = $("#student-template").html(),
-
-//                 template = Handlebars.compile(templateSource),
-
-//                 studentHTML = template(resJSON);
-
-//            $('#my-container').html(studentHTML);
-//            // console.log($("#student-template"))
-//         },
-//         loadStudentData : function(){
-//         	$(".ajax-loader").removeClass("hide");
-//         	setTimeout(function(){
-//         		  $.ajax({
-//                 url:"http://localhost/SocialGraph/data/studentData.json",
-//                 method:'get',
-//                 success:function(resJSON){
-//                 	$(".ajax-loader").addClass("hide");
-//                 	STUDENT_METHOD.handlerData(resJSON);
-//                 }
-//             	})
-//         	},1000);
-//         }
+/* activate scrollspy menu */
+$('body').scrollspy({
+  target: '#navbar-collapsible',
+  offset: 52
 });
+
+/* smooth scrolling sections */
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 50
+        }, 800);
+        
+        if (this.hash=="#section1") {
+            $('.scroll-up').hide();
+        }
+        else {
+            $('.scroll-up').show();
+        }
+        
+        
+        // activte animations in this section
+        target.find('.animate').delay(1200).addClass("animated");
+        setTimeout(function(){
+            target.find('.animated').removeClass("animated");
+        },2000);
+        
+        return false;
+      }
+    }
+});
+
+$(document).ready(function(){
+	$(".fa.fa-facebook").on("click",function(){
+		
+	});
+});
+
+
+
+
+
+
